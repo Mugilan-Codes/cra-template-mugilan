3962bf9d55a38cf39e9462a1459e21575698b047
Mugilan <mugilancodes@gmail.com>
2021-12-28 23:18:13 +0530

    add pages :lipstick:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/Home';
+import AboutPage from './pages/About';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='about' element={<AboutPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+const AboutPage = () => {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      <nav>
+        <Link to='/'>Home</Link>
+      </nav>
+    </>
+  );
+};
+
+export default AboutPage;
