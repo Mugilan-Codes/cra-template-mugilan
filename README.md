1699db3fbdc999aafdd535ed29eff756b177617b
Mugilan <mugilancodes@gmail.com>
2021-12-29 00:41:15 +0530

    add react-router-bootstrap :heavy_plus_sign:

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ npx create-react-app my-app --template cra-template-mugilan
 
 - [React Router](https://reactrouter.com/)
 - [React Bootstrap](https://react-bootstrap.github.io/)
+- [React Router Bootstrap](https://github.com/react-bootstrap/react-router-bootstrap)
 
 #### Inspiration
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 const HomePage = () => {
   return (
     <>
       <main>
         <h2>Welcome to the homepage!</h2>
+
         <p>You can do this, I believe in you.</p>
+
+        <LinkContainer to='/about'>
+          <Button>Go To About</Button>
+        </LinkContainer>
       </main>
+
       <nav>
         <Link to='/about'>About</Link>
       </nav>
